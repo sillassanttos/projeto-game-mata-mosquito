372438426e3f14c50e5561c2eaae688970c93f37
Silas Santos <sillassanttos@outlook.com>
2023-02-21 15:10:33 -0400

    Adicionado lado aleatório do mosquito

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ function posicaoRandomica() {
 
   var mosquito = document.createElement('img')
   mosquito.src = '../../assets/img/mosquito.png'
-  mosquito.className = tamanhoAleatorio()
+  mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
   mosquito.style.left = posicaoX + 'px'
   mosquito.style.top = posicaoY + 'px'
   mosquito.style.position = 'absolute'
@@ -39,6 +39,15 @@ function tamanhoAleatorio() {
     case 0: return 'mosquito1'
     case 1: return 'mosquito2'
     case 2: return 'mosquito3'
+  }
+}
+
+function ladoAleatorio() {
+  var classe = Math.floor(Math.random() * 2)
+
+  switch (classe) {
+    case 0: return 'ladoA'
+    case 1: return 'ladoB'
   }
 }
 
